e3d83d9c5501b2f11ab60b2685570d0e189826a6
TheNameIsArt <62926983+TheNameIsArt@users.noreply.github.com>
2023-05-07 21:09:34 +0200

    filer og navigation

---
+++ script.js
@@ -0,0 +1,10 @@
+// Menu button
+$(document).ready(function()
+{
+    // When pressing the #menu icon,
+    $('#menu').click(function()
+    {
+        // the 'ul' list will show
+        $('ul').toggleClass('show');
+    });
+});
