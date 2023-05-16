bff17a1c5467e2bba22970ba2e61c91200e089b4
TheNameIsArt <anya.r.tandrup@gmail.com>
2023-05-16 22:02:14 +0200

    update

--- script.js
+++ script.js
@@ -55,3 +55,19 @@ function search_tut() {
       }
   }
 }
+
+// Search bar - multimediedesigner
+function search_mul() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('mul-info');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
